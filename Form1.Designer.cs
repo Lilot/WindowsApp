6548428a267c9b5d1ac9fadddfdf81d5d5f337ba
Amparo Smith <Lilot@users.noreply.github.com>
2020-06-05 16:34:02 -0400

    add files to upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RandomGenerator
+{
+    partial class FrmRandGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenRand = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenRand
+            // 
+            this.btnGenRand.Location = new System.Drawing.Point(151, 250);
+            this.btnGenRand.Name = "btnGenRand";
+            this.btnGenRand.Size = new System.Drawing.Size(272, 126);
+            this.btnGenRand.TabIndex = 0;
+            this.btnGenRand.Text = "Shuffle and Read";
+            this.btnGenRand.UseVisualStyleBackColor = true;
+            this.btnGenRand.Click += new System.EventHandler(this.btnGenRand_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(146, 139);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(307, 75);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = " Take a Deep Calming Breath.\r\n Concentrate on your question.\r\n Your in-depth Read" +
+    "ing";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // FrmRandGen
+            // 
+            this.ClientSize = new System.Drawing.Size(579, 552);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnGenRand);
+            this.Name = "FrmRandGen";
+            this.Text = "Mahjong Divination App";
+            this.Load += new System.EventHandler(this.FrmRandGen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumGen;
+        private System.Windows.Forms.TextBox txtBxResult;
+        private System.Windows.Forms.Label Titlelbl;
+        private System.Windows.Forms.Button btnGenRand;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.Random;
+using MathNet.Numerics.Distributions;
+using System.IO;
+
+
+namespace RandomGenerator
+{
+    public partial class FrmRandGen : Form
+    {
+        public FrmRandGen()
+        {
+            InitializeComponent();
+            CalNumber() ;
+            ReadTile();
+        }
+
+        public static string Tile1 { get; private set; }
+        public static string Tile2 { get; private set; }
+
+        public static string Tile3 { get; private set; }
+
+        private static string TileMeaning = @"..\..\..\..\..\MyAppC#\MahjongDivinationApp\MahjongOracle.txt";
+
+
+        private static int CalNumber()
+        {
+            
+            {
+                Random random = new SystemRandomSource();
+                 decimal MahjongNumber = (random.NextDecimal() * 42) + 1;
+                Math.Round(MahjongNumber);
+                return (int)MahjongNumber;
+               
+
+
+            }
+        }
+
+        private static string ReadTile()
+        {
+
+            string line = null;
+            int lineNumber = CalNumber();
+            try
+            {
+
+                using (var text = new StreamReader(TileMeaning))
+                {
+                    text.ReadLine();
+                    for (int i = 1; i < lineNumber; i++)
+                    {
+                        text.ReadLine();
+                        if (text.EndOfStream)
+                        {
+                            Console.Write($"End of File. The file contains only this number {i} of lines. ");
+                            break;
+                        }
+
+                    }
+                    line = text.ReadLine();
+                }
+
+
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("There is an error in reading the file.");
+                Console.WriteLine(e.Message);
+            }
+
+            return line;
+        }
+        private void btnGenRand_Click(object sender, EventArgs e)
+        {
+            Tile1 = ReadTile();
+            Tile2 = ReadTile();
+            Tile3 = ReadTile();
+
+            //compare Tile1 and Tile2
+            if ((Tile1 != Tile2) && (Tile1 != Tile3) && (Tile2 != Tile3))
+            {
+                MessageBox.Show( "Your Present, Past and Future Mahjong Reading: " + "\n" + "\n"+
+                                 "Past   : " + Tile1 + "\n "+ "\n" +
+                                "Present  : " + Tile2 + "\n "+ "\n" +
+                                "Future:   : " + Tile3);
+
+               
+            }
+            
+           
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmRandGen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
